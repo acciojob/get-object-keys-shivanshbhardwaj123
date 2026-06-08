451d386d1e85393b3412b2ed98f99c3cf9bf7267
shivanshbhardwaj123 <shivanshgaurav1234@gmail.com>
2026-06-09 03:39:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const student={name:"sonu",
+			    age:23
+	}
+	function getkeys(obj){
+		//const keys=object.keys(student)
+		return object.keys(student);
+	}
+console.log(getkeys(student));
+
